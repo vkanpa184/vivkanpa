5a06a3a3870a7047d3abed389fe1b0ee2a1395de
vkanpa184 <69128428+vkanpa184@users.noreply.github.com>
2023-12-08 23:18:00 +0000

    DECEMBER UPDATES

--- js/resume.js
+++ js/resume.js
@@ -26,3 +26,35 @@
   });
 
 })(jQuery); // End of use strict
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom + 0.05*objectBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(250,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(50,0);}
+      }
+
+
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
+
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
+  $('.header-overlay').css({
+    opacity: function() {
+      var elementHeight = $(this).height();
+      return (4*elementHeight - 0.5*scrollTop) / elementHeight*0.3;
+    }
+  });
+
+});
